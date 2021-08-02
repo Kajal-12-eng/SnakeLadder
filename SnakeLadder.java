16e2948b9ecc3f4dfced5954cda7cff7b1de2898
kajal-12-eng <kajalsingh85654@gamil.com>
2021-08-02 20:50:05 +0530

    Random Value of die

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,5 +5,8 @@ public class SnakeLadder {
 		System.out.println("Welcome to snake and Ladder Game");
 		int playerPosition = 0;
 		System.out.println("starting position of the player is:" + playerPosition);
+		
+		int die = (int) (Math.floor(Math.random() *10)%6+1);
+		System.out.println("Die rolled is:" + die );
 	}
 }
