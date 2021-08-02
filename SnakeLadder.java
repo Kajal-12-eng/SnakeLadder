4b21a5839d5e23204c2eb565f0feb302f702c34a
kajal-12-eng <kajalsingh85654@gamil.com>
2021-08-02 20:39:33 +0530

    player1 position is added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,5 +3,7 @@ package com.sankelad;
 public class SnakeLadder {
 	public static void main (String[] args) {
 		System.out.println("Welcome to snake and Ladder Game");
+		int playerPosition = 0;
+		System.out.println("starting position of the player is:" + playerPosition);
 	}
 }
