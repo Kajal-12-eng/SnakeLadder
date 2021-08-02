007a0088c551003f964905f8f78deb4dcb7f3c00
kajal-12-eng <kajalsingh85654@gamil.com>
2021-08-03 00:56:45 +0530

    Iterating the loop till player reaches winning position 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,12 +1,33 @@
 package com.sankelad;
 
 public class SnakeLadder {
+	
 	public static void main (String[] args) {
 		System.out.println("Welcome to snake and Ladder Game");
 		int playerPosition = 0;
-		System.out.println("starting position of the player is:" + playerPosition);
-		
+		int winningPosition = 100;
+		while(winningPosition > playerPosition)
+		{
 		int die = (int) (Math.floor(Math.random() *10)%6+1);
 		System.out.println("Die rolled is:" + die );
+		
+		int randomCheck = (int) (Math.floor(Math.random() *10)%3);
+		  switch(randomCheck) {
+		  case 0 :
+		  	System.out.println("no play");
+		  	playerPosition  += 0;
+			break;
+		 case 1:
+			System.out.println("ladder");
+			playerPosition  += die;
+			break;
+		 case 2:
+			System.out.println("Snake") ;
+			playerPosition  -= die;
+			if (playerPosition < 0)
+				playerPosition = 0;
+		  }
+		}
+		  System.out.println("player position is :" + playerPosition);
 	}
 }
